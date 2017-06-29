7a1bdb70df05768bde6012bad149d04b895ab47e
Patrick Luck <stonesoup@Patricks-MacBook-Pro.local>
2017-06-29 12:28:40 -0400

    Code clean up and comments

--- src/main/kotlin/com/stonesoupprogramming/jdbc/SpringJdbcApplication.kt
+++ src/main/kotlin/com/stonesoupprogramming/jdbc/SpringJdbcApplication.kt
@@ -6,9 +6,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.jdbc.core.JdbcOperations
 import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.core.RowMapper
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
@@ -24,14 +22,19 @@ import javax.sql.DataSource
 @SpringBootApplication
 class SpringJdbcApplication
 
+//Define a data class that maps to both our
+//form and database table
 data class User(var firstName: String = "",
                 var lastName: String = "",
                 var email: String = "",
                 var phone: String = "")
 
+
 @Configuration
 class Configuration {
 
+    //First configure a data source that
+    //generates an embedded db
     @Bean(name = arrayOf("dataSource"))
     fun dataSource(): DataSource {
 
@@ -42,6 +45,7 @@ class Configuration {
                 .build()
     }
 
+    //Create a JdbcTemplate Bean that connects to our database
     @Bean
     fun jdbcTemplate(@Qualifier("dataSource") dataSource: DataSource): JdbcTemplate {
         return JdbcTemplate(dataSource)
@@ -86,6 +90,8 @@ class IndexRepository(@Autowired var jdbcTemplate: JdbcTemplate) {
 
 
     fun addUser(user: User) {
+        //We can use SimpleJdbcInsert to insert a value into our table
+        //The becomes super concise when combined with Kotlins apply and mapOf functions
         SimpleJdbcInsert(jdbcTemplate).withTableName("USERS").apply {
             setGeneratedKeyName("id")
             execute(
@@ -96,6 +102,9 @@ class IndexRepository(@Autowired var jdbcTemplate: JdbcTemplate) {
         }
     }
 
+    //This allows us to query the Users table and return a list of users
+    //This is one method call to jdbcTemplate with a lambda expression which makes the code
+    //incredibly concise
     fun allUsers(): List<User> = jdbcTemplate.query("SELECT FIRST_NAME, LAST_NAME, EMAIL, PHONE FROM USERS",
             { rs: ResultSet, _: Int ->
                 User(rs.getString("first_name"), rs.getString("last_name"), rs.getString("email"), rs.getString("phone"))
